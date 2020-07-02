104085a56aa4b63d42374686ac33dd9ed1c253fd
7000WORDS <60527853+7000WORDS@users.noreply.github.com>
2020-07-02 14:18:44 +0100

    Update proj.py

--- proj.py
+++ proj.py
@@ -7,6 +7,14 @@ else:
   print("x is n not equal to y")
   
 def myfunc():
-  n = input
+  n = input()
   return n * n
 myfunc()
+
+import random
+x = random.randrange(1,  10)
+u = input()
+if u = x:
+  print("you have guessed it right")
+else:
+  print("you got guessed it, WRONG ")
